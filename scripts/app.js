23fafeb1d6c4801c5b952cdb3ca68af449a17e59
nadavys <nadavys@yahoo.com>
2015-07-02 17:37:38 -0700

    0702d

--- scripts/app.js
+++ scripts/app.js
@@ -252,7 +252,9 @@ angular.module('stormpathIdpApp').controller('VerifyCtrl', [
 angular.module('stormpathIdpApp').controller('ErrorCtrl', [
   '$scope',
   'Stormpath',
-  function ($scope, Stormpath) {
+  'cb_uri',
+  function ($scope, Stormpath, cb_uri) {
+    console.log('cb_uri', cb_uri);
     $scope.errors = Stormpath.errors;
     $scope.inError = false;
     // todo: uncomment!!!!
